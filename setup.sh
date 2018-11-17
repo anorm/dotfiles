43b56ec05d7f671b4e7f81b9a9701340684e2022
Anders Norman <anders.norman@barco.com>
2018-11-17 21:38:50 +0100

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+MODULES=(vim tmux zsh)
+
+# Install prerequisite packages
+for MODULE in "${MODULES[@]}"; do
+    cat modules/$MODULE/packages.apt 2>/dev/null
+done | sort -u | sudo xargs apt install -y
+
+# Run module setup scripts
+for MODULE in "${MODULES[@]}"; do
+    modules/$MODULE/setup.sh
+done
