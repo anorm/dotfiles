4a3c4164da8bfff445fb235adfff9dc5c2bf2594
Anders Norman <anders.norman@simployer.com>
2020-06-25 12:27:32 +0200

    Added initial docker-cli

---
+++ modules/docker-cli/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dpkg --get-selections | grep -q docker-cli && exit
+
+# Download and add Docker's official public PGP key.
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add the `stable` channel's Docker upstream repository.
+#
+# If you want to live on the edge, you can change "stable" below to "test" or
+# "nightly". I highly recommend sticking with stable!
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# Update the apt package list (for the new apt repo).
+sudo apt-get update -y
+exit
+# Install the latest version of Docker CE.
+sudo apt-get install -y docker-ce
+
+# Allow your user to access the Docker CLI without needing root access.
+sudo usermod -aG docker $USER
