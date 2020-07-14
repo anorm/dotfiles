2aeda989f86c84e2652f9bb7ffddfe0e80590522
Anders Norman <anders.norman@simployer.com>
2020-07-14 15:48:16 +0200

    Added googler module

---
+++ modules/googler/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+DIR="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
+
+GIT_URL=https://github.com/jarun/googler.git
+GIT_DIR=$DIR/git
+if [ ! -e $GIT_DIR ]; then
+    git clone $GIT_URL $GIT_DIR
+fi
+mkdir -p ~/bin
+ln -fs $DIR/git/googler ~/bin
