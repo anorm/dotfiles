cdec669c2cb97e9906a272e75f0ac1b168b05376
Anders Norman <anders@norman.info>
2023-09-30 12:14:01 +0200

    Fixed testing if current shell is zsh even through symlink

--- modules/zsh/setup.sh
+++ modules/zsh/setup.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 
 ln -fs $DIR/.zshrc ~
 
-if [ "$SHELL" != "$(which zsh)" ]; then
+if [ ! "$SHELL" -ef "$(which zsh)" ]; then
     echo "Gonna change the SHELL to $(which zsh). You'll be prompted for your password..."
     chsh -s $(which zsh)
 fi
