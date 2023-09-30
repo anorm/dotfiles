e0090e1d6e8ec899d23a9aec300a33df7f6b13c0
Anders Norman <anders@norman.info>
2023-09-30 12:14:01 +0200

    Adding git

---
+++ modules/git/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+if ! git config --global --get user.email >/dev/null; then
+    echo -n 'Enter email address to use for ~/.gitconfig: '
+    read email
+    git config --global user.email $email
+fi
+
+if ! git config --global --get user.name >/dev/null; then
+    echo -n 'Enter name to use for ~/.gitconfig: '
+    read name
+    git config --global user.name "$name"
+fi
+
+if ! git config --global --get alias.st >/dev/null; then
+    git config --global alias.st "status -bs"
+fi
