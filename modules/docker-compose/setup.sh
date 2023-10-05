fb8d5fce2038388d990f90896203ecf70c6abc48
Anders Norman <anders@norman.info>
2023-10-05 16:28:15 +0200

    Deleted docker-compose module

--- modules/docker-compose/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-URL=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r ".assets[].browser_download_url | select((. | contains(\"$(uname -s)-$(uname -m)\")) and (. | contains(\"sha256\") | not))")
-curl -L $URL -o /usr/local/bin/docker-compose
-chmod 755 /usr/local/bin/docker-compose
