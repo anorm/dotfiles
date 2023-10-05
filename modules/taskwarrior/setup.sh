2fb3525a86f95b926d6bff2bfc43f67c13ef0e6d
Anders Norman <anders@norman.info>
2023-10-05 16:46:02 +0200

    Deleted taskwarrior module

--- modules/taskwarrior/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
-ln -fs $DIR/.taskrc ~
-ln -fs $DIR/.task ~
-mkdir -p ~/.bashrc.d
-ln -fs $DIR/taskwarrior.bashrc ~/.bashrc.d/
-echo "INFO: Remember to copy your taskwarrior freecinc keys to ~/.task/ and do the necessary changes to ~/.task/taskd.conf."
