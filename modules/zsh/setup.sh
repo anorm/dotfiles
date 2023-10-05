f6d9922ef6d19f55c97e8ed660c9a3a69962c58a
Anders Norman <anders@norman.info>
2023-10-05 16:54:07 +0200

    Cleaned up zsh

--- modules/zsh/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
-
-ln -fs $DIR/.zshrc ~
-
-if [ ! "$SHELL" -ef "$(which zsh)" ]; then
-    echo "Gonna change the SHELL to $(which zsh). You'll be prompted for your password..."
-    chsh -s $(which zsh)
-fi
