7bd82b084269f3c7e749d2ecf11590bce3091dbc
Anders Norman <anders@norman.info>
2024-12-19 07:13:49 +0100

    nvim tweaks

--- modules/nvim/config/init.lua
+++ modules/nvim/config/init.lua
@@ -1,11 +1,7 @@
--- Use comma as Leader
-vim.g.mapleader = ','
-
--- Make TAB completion use longest unique match (not first partial match)
-vim.o.wildmode = 'longest,list'
-
--- Line numbers
-vim.wo.number = true
+vim.g.mapleader = ','            -- Use comma as Leader
+vim.o.wildmode = 'longest,list'  -- Make TAB completion use longest unique match (not first partial match)
+vim.wo.number = true             -- Show line numbers
+vim.o.wrapscan = false           -- Make search stop at EOF and not wrap to top
 
 -- Key mappings
 vim.keymap.set("n", "<leader><leader>", "<cmd>Files<cr>")
