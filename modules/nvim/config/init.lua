03dbfe818a5597edae5a4e6c5881eaec9de3d6d3
Anders Norman <anders@norman.info>
2025-02-25 11:49:05 +0100

    Ghostty and colors

--- modules/nvim/config/init.lua
+++ modules/nvim/config/init.lua
@@ -1,7 +1,21 @@
-vim.g.mapleader = ','            -- Use comma as Leader
-vim.o.wildmode = 'longest,list'  -- Make TAB completion use longest unique match (not first partial match)
-vim.wo.number = true             -- Show line numbers
-vim.o.wrapscan = false           -- Make search stop at EOF and not wrap to top
+vim.g.mapleader = ','              -- Use comma as Leader
+vim.opt.wildmode = 'longest,list'  -- Make TAB completion use longest unique match (not first partial match)
+vim.opt.number = true              -- Show line numbers
+vim.opt.wrapscan = false           -- Make search stop at EOF and not wrap to top
+vim.opt.cursorline = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 -- Key mappings
 vim.keymap.set("n", "<leader><leader>", "<cmd>Files<cr>")
+
+-- Color scheme
+vim.opt.background = "dark"
+require("gruvbox").setup({
+  contrast = "hard",
+})
+vim.cmd([[colorscheme gruvbox]])
+
+-- Init oil
+require("oil").setup()
