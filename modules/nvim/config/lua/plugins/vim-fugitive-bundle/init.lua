b863dec67a0daf229693af4a2a71be3d8695f5c6
Anders Norman <anders@norman.info>
2025-02-25 22:50:20 +0100

    Reworked nvim config

---
+++ modules/nvim/config/lua/plugins/vim-fugitive-bundle/init.lua
@@ -0,0 +1,3 @@
+-- Make sure nvim can find the plugin
+local current_dir = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":h")
+vim.opt.runtimepath:append(current_dir .. "/submodules/vim-fugitive")
