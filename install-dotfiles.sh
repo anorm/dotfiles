58da68b5986e3160ed371ee138c269debe23ccb5
Anders Norman <anders@norman.info>
2025-02-25 17:37:27 +0100

    Improved the install-dotfiles.sh script to gracefully handle directory symlinks

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -4,16 +4,36 @@ set -eu
 
 DIR="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 
+# Loop over all 'dotfiles' directories
 while read MODULE_DOTFILES; do
     echo "Installing dotfiles: $MODULE_DOTFILES"
     (
+        # Enter the dotfile directory
         cd $MODULE_DOTFILES
+
+        # Loop over all files and symlinks
         while read FILENAME; do
-            DIRNAME=$(dirname $FILENAME)
-            FILENAME=${FILENAME:2}
-            mkdir -p ~/$DIRNAME
+            DIRNAME="$(dirname $FILENAME)"
+            FILENAME="${FILENAME:2}"
+            mkdir -p "${HOME}/$DIRNAME"
             echo "  ~/$FILENAME"
-            ln -sf $DIR/$MODULE_DOTFILES/$FILENAME ~/$FILENAME
+
+            # Does the target already exist?
+            if [[ -e "${HOME}/$FILENAME" ]]; then
+                # Make sure it's not a regular file
+                if [[ ! -L "${HOME}/$FILENAME" ]]; then
+                    echo -e "\033[0;31mERROR: ~/$FILENAME already exists and is not a symlink\033[0m"
+                    exit 1
+                fi
+
+                # Delete the old link (this must be done explicitly, otherwise, the new
+                # symlink will be created _inside_ the target if the target is a
+                # directory)
+                rm "${HOME}/$FILENAME"
+            fi
+
+            # Create the symlink
+            ln -fs "$DIR/$MODULE_DOTFILES/$FILENAME" "${HOME}/$FILENAME"
         done < <(find . -not -type d)
     )
 done < <(find modules -type d -maxdepth 2 -name dotfiles)
