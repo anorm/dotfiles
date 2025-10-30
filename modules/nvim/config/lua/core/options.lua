48c00c27b8a4c369a80f624ee0e54ba61c4f3d71
Anders Norman <anders@norman.info>
2025-10-30 12:15:56 +0100

    nvim config

--- modules/nvim/config/lua/core/options.lua
+++ modules/nvim/config/lua/core/options.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = ','              -- Use comma as Leader
 
 vim.opt.wildmode = 'longest,list'  -- Make TAB completion use longest unique match (not first partial match)
+vim.opt.completeopt = {"menu", "fuzzy", "longest"}
 vim.opt.number = true              -- Show line numbers
 vim.opt.wrapscan = false           -- Make search stop at EOF and not wrap to top
 vim.opt.cursorline = true
