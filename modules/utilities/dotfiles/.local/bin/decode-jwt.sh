5544cc0751f15f3961b971d13396d187d71895ee
Anders Norman <anders@norman.info>
2026-01-14 08:46:17 +0100

    Fixed a lot of shellcheck feedback

--- modules/utilities/dotfiles/.local/bin/decode-jwt.sh
+++ modules/utilities/dotfiles/.local/bin/decode-jwt.sh
@@ -3,22 +3,22 @@
 set -e
 
 TOKEN=$(cat)
-HEADER=$(   echo -n $TOKEN | ( cut -d. -f1; echo -n == ) | base64 -d)
-PAYLOAD=$(  echo -n $TOKEN | ( cut -d. -f2; echo -n == ) | base64 -d)
-SIGNATURE=$(echo -n $TOKEN | cut -d. -f3)
+HEADER=$(   echo -n "$TOKEN" | ( cut -d. -f1; echo -n == ) | base64 -d)
+PAYLOAD=$(  echo -n "$TOKEN" | ( cut -d. -f2; echo -n == ) | base64 -d)
+SIGNATURE=$(echo -n "$TOKEN" | cut -d. -f3)
 
 (
     echo -n '{'
     echo -n '"header":'
-    echo -n $HEADER
+    echo -n "$HEADER"
     echo -n ","
 
     echo -n '"payload":'
-    echo -n $PAYLOAD
+    echo -n "$PAYLOAD"
     echo -n ","
 
     echo -n '"signature": "'
-    echo -n $SIGNATURE
+    echo -n "$SIGNATURE"
     echo -n '"'
 
     echo -n '}'
