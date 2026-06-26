49ad2fa9c3d2ca179b8b27bfa227f32242ade2a1
Anders Norman <anders@norman.info>
2026-06-26 08:27:35 +0200

    nvim: Add leader-G for whole word git grep

--- modules/nvim/config/lua/plugins/vim-fugitive-bundle/init.lua
+++ modules/nvim/config/lua/plugins/vim-fugitive-bundle/init.lua
@@ -3,20 +3,35 @@ local current_dir = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":h"
 vim.opt.runtimepath:append(current_dir .. "/submodules/vim-fugitive")
 
 -- Key mappings
-local function ggrep(text)
+local function ggrep(text, whole_word)
   if not text or text == "" then
     return
   end
-  vim.cmd("silent! Ggrep! -F -- " .. vim.fn.shellescape(text))
+
+  local flags = "-F"
+  if whole_word then
+    flags = flags .. " -w"
+  end
+
+  vim.cmd("silent! Ggrep! " .. flags .. " -- " .. vim.fn.shellescape(text))
   vim.cmd("copen")
   vim.cmd("wincmd p")
 end
 
+vim.keymap.set("n", "<leader>G", function()
+  ggrep(vim.fn.expand("<cword>"), true)
+end, { desc = "Ggrep whole word under cursor" })
+
 vim.keymap.set("n", "<leader>g", function()
-  ggrep(vim.fn.expand("<cword>"))
+  ggrep(vim.fn.expand("<cword>"), false)
 end, { desc = "Ggrep word under cursor" })
+
+vim.keymap.set("x", "<leader>G", function()
+  local selected_text = vim.fn.getregion(vim.fn.getpos("v"), vim.fn.getpos("."))[1]
+  ggrep(selected_text, true)
+end, { desc = "Ggrep whole visual selection" })
 
 vim.keymap.set("x", "<leader>g", function()
   local selected_text = vim.fn.getregion(vim.fn.getpos("v"), vim.fn.getpos("."))[1]
-  ggrep(selected_text)
+  ggrep(selected_text, false)
 end, { desc = "Ggrep visual selection" })
