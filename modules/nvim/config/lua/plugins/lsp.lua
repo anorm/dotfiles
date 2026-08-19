e224029f75242428a804f030588c55797caf5c48
Anders Norman <anders@norman.info>
2026-08-19 10:08:51 +0200

    nvim: Add clangd LSP

--- modules/nvim/config/lua/plugins/lsp.lua
+++ modules/nvim/config/lua/plugins/lsp.lua
@@ -26,6 +26,7 @@ return {
 
             require("mason-lspconfig").setup({
                 ensure_installed = {
+                    "clangd",
                     "pyright",
                     "rust_analyzer",
                     "terraformls",
