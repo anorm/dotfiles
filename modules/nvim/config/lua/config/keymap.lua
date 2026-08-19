6a0be67f6a443261ec8348304c11d52653e864a9
Anders Norman <anders@norman.info>
2026-08-19 09:53:59 +0200

    nvim: map å/¨ into [/] for easier norwegian keyboard access

--- modules/nvim/config/lua/config/keymap.lua
+++ modules/nvim/config/lua/config/keymap.lua
@@ -1,6 +1,11 @@
 vim.g.mapleader = ","
 vim.g.maplocalleader = "\\"
 
+-- Norwegian keyboard hack
+-- Make 'å' and '¨' act as [ and ] in normal mode
+vim.keymap.set("n", "å", "[", { remap = true })
+vim.keymap.set("n", "¨", "]", { remap = true })
+
  -- Highlight word under cursor like `*`, but don't jump.
  -- Still sets the last-search pattern, so `n`/`N` work.
  vim.keymap.set("n", "+", function()
